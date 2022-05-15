6b8e4989dec23317669d976f296176d4508bb6d5
SputnikTH <105562195+SputnikTH@users.noreply.github.com>
2022-05-15 20:50:35 +0700

    Complete Excesice 4

---
+++ Exercise4_Rachata_Wjp.py
@@ -0,0 +1,14 @@
+e = "Foundation Enlish"
+g = "General Business"
+i = "Introduction to Computer Systems"
+c = "Computer Programming"
+esc = 40
+gsc = 80
+isc = 100
+csc = 100
+print(e,esc,)
+print(g,gsc)
+print(i,isc)
+print(c,csc)
+print("---------------------------------")
+print("Total Score",esc+gsc+isc+csc)
